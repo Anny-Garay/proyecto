f530c7de1aca2283329c75f89a3ff5d5108017d3
annMiin <annyloyz@hotmail.com>
2022-07-06 22:33:02 -0500

    actualizacion

--- wp-content/themes/proyecto/front-page.php
+++ wp-content/themes/proyecto/front-page.php
@@ -9,12 +9,12 @@
 </div>
     <div class="your-class1">
       
-      <div><a href="#" style="background-image:url(<?php the_field ('populares_1');?>)"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('populares_2');?>)"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('populares_3');?>"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('populares_4');?>"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('populares_5');?>"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('populares_6');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_1');?>" style="background-image:url(<?php the_field ('populares_1');?>)"></a></div>
+      <div><a href="<?php the_field ('enlace_2');?>" style="background-image:url(<?php the_field ('populares_2');?>)"></a></div>
+      <div><a href="<?php the_field ('enlace_3');?>" style="background-image:url(<?php the_field ('populares_3');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_4');?>" style="background-image:url(<?php the_field ('populares_4');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_5');?>" style="background-image:url(<?php the_field ('populares_5');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_6');?>" style="background-image:url(<?php the_field ('populares_6');?>"></a></div>
 
 
     </div>
@@ -48,12 +48,12 @@
     <button role="button" id="flecha-izquierda3" class="flecha-izquierda3"><i class="fas fa-angle-left"></i></button>
   <button role="button" id="flecha-derecha3" class="flecha-derecha3"><i class="fas fa-angle-right"></i></button>
       <div class="your-class3">
-      <div><a href="#" style="background-image:url(<?php the_field ('comedia_1');?>)"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('comedia_2');?>)"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('comedia_3');?>"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('comedia_4');?>"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('comedia_5');?>"></a></div>
-      <div><a href="#" style="background-image:url(<?php the_field ('comedia_6');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_1');?>" style="background-image:url(<?php the_field ('comedia_1');?>)"></a></div>
+      <div><a href="<?php the_field ('enlace_2');?>" style="background-image:url(<?php the_field ('comedia_2');?>)"></a></div>
+      <div><a href="<?php the_field ('enlace_3');?>" style="background-image:url(<?php the_field ('comedia_3');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_4');?>" style="background-image:url(<?php the_field ('comedia_4');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_5');?>" style="background-image:url(<?php the_field ('comedia_5');?>"></a></div>
+      <div><a href="<?php the_field ('enlace_6');?>" style="background-image:url(<?php the_field ('comedia_6');?>"></a></div>
 
    </div>
   </div> 
