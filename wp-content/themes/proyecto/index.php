a976422e602f8066dcce48123abcffc241ce71f7
annMiin <annyloyz@hotmail.com>
2022-07-18 01:09:29 -0500

    mori

--- wp-content/themes/proyecto/index.php
+++ wp-content/themes/proyecto/index.php
@@ -1,13 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-<body>
+  
+<?php get_header();?>
+
+    <section>
+      <div class="banner">
+        <div><img src="https://archivos-cms.cinecolombia.com/images/_aliases/exhibition_embed/0/8/7/5/25780-1-esl-CO/eec0cd20f0fa-imagen_trailer_1000x510.jpg" width="100%"  height="450"></div>
+        <div><img src="https://lafrikileria.com/blog/wp-content/uploads/2018/04/IW.jpg" width="100%"  height="450"  alt=""></div>
+        <div><img src="https://tumbaabierta.com/wp-content/uploads/2012/02/ira_de_titanes_wrath_titans_1920x1200_wallpaper_1.jpg"  width="100%"  height="450"></div>
+      </div>
     
-</body>
-</html>
+      <h3>Peliculas Exitosas</h3>
+        
+            <div class="multiple-items">
+              <div><img src="https://s3.cine3.com/2020/06/titans2.jpg" width="280" height="350" alt=""></div>
+              <div><img src="https://http2.mlstatic.com/D_NQ_NP_760751-MLM41788706380_052020-W.jpg" width="280" height="350" alt=""></div>
+              <div><img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/480/public/media/image/2020/09/poster-batman-2021-2064381.jpg?itok=SEuYP8hx" width="280"  height="350" alt=""></div>
+              <div><img src="https://www.ecured.cu/images/3/3b/Guerra-mundial-z-.jpg" width="280" height="350" alt=""></div>
+              <div><img src="https://es.web.img2.acsta.net/medias/nmedia/18/80/30/27/19711024.jpg" width="280" height="350" alt=""></div>
+              <div><img src="https://i.postimg.cc/g04V5wfH/as.jpg" width="280" height="350" alt=""></div>
+           </div>
+        
+      
+      
+         
+      
+      <h3>Proximamente</h3>
+      <div class="lazy">
+      
+        <div><img src="https://bonitanetworkmedia.s3.amazonaws.com/2022/05/r7UQvJcB-Avatar-El-Camino-del-Agua.jpg" width="280" height="350" alt=""></div>
+        <div><img src="https://lumiere-a.akamaihd.net/v1/images/1_cb8bc30f.jpeg" width="280" height="350" alt=""></div>
+        <div><img src="https://pm1.narvii.com/6103/5c2c235eccc34e929cebc01c2bb2100c02632a99_hq.jpg" width="280" height="350" alt=""></div>
+        <div><img src="https://cdn.kinocheck.com/images/w350/rwb5votlfw.jpg"width="280" height="350" alt=""></div>
+        <div><img src="http://pulpfictioncine.com/download/multimedia.normal.82f4a5fc14dc9844.7368617a616d2d706f737465722d315f6e6f726d616c2e6a7067.jpg" width="280" height="350" alt=""></div>
+        <div><img src="https://s3-eu-west-1.amazonaws.com/abandomedia/db/foto/db_8531_49.jpg" width="280" height="350" alt=""></div>
+      
+      </div>
+</section>
+           
+<?php get_footer();?>
+    
